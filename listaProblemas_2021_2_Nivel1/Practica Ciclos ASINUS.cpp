409a21efceccc3b93d2e0f7a5dfaf1196dc3d2af
Dienicx <Diegolanderos84@gmail.com>
2022-01-16 19:09:47 -0600

    .

---
+++ listaProblemas_2021_2_Nivel1/Practica Ciclos ASINUS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+int main () {
+					//En este codigo, solo en cpp17-gcc me sale un AC en OmegaUp, por motivo que desconozco.
+int N,n,r;
+cin >>N;
+for (int i=1;i<=N;i++){
+
+	cin >>n;
+		r=r+n;
+	
+}
+cout <<r<<endl;
+	return 0;
+}
