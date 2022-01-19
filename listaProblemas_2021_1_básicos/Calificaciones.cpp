49d95641330dd6955055a70ff7f094e25a00421c
Dienicx <Diegolanderos84@gmail.com>
2022-01-18 20:53:12 -0600

    .

---
+++ listaProblemas_2021_1_básicos/Calificaciones.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+int main () {
+int a,b,c;
+cin >>a>>b>>c;
+
+if(((a+b+c)/3)>=6)
+	{
+		cout <<1;
+	}
+else
+	{
+		cout <<0;
+	}
+return 0;
+}
